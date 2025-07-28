6c9fa2738d091b316f69d4d1ce4c4e8d65e0cf90
chitranshjawre <chitranshjawre2001@gmail.com>
2025-07-28 22:03:39 +0530

    update

--- src/Features/store/store.js
+++ src/Features/store/store.js
@@ -1,13 +1,11 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../carts/Cartslice.js';
 import productReducer from '../Produtcs/productSlice.js';
 import buyNowreducer from '../BuyNow/buyNowSlice.js';
 import authReducer from '../Auth/authSlice.js';
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
     products: productReducer,
     buyNow: buyNowreducer,
     auth: authReducer,
