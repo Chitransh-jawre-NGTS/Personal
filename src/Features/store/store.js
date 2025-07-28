42b68600ae88ad9e5b905ddc703897fb1a67e554
chitranshjawre <chitranshjawre2001@gmail.com>
2025-07-28 21:40:59 +0530

    update

--- src/Features/store/store.js
+++ src/Features/store/store.js
@@ -3,16 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../carts/Cartslice.js';
 import productReducer from '../Produtcs/productSlice.js';
 import buyNowreducer from '../BuyNow/buyNowSlice.js';
-import authReducer from "../Auth/authSlice.js"
-import { BugPlay } from 'lucide-react';
-import { b } from 'framer-motion/client';
-
+import authReducer from '../Auth/authSlice.js';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    products: productReducer, 
-    buyNow: buyNowreducer, 
+    products: productReducer,
+    buyNow: buyNowreducer,
     auth: authReducer,
   },
 });
